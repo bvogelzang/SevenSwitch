55e656f0db8e3f16724ab83cc5c0f0e1ded3254f
bvogelzang <bvogelzang@breuer.com>
2014-06-20 14:28:05 -0500

    start the swift conversion process

---
+++ SevenSwitchExample/SevenSwitchExample/SevenSwitch.swift
@@ -0,0 +1,53 @@
+//
+//  SevenSwitch.swift
+//
+//  Created by Benjamin Vogelzang on 6/20/14.
+//  Copyright (c) 2014 Ben Vogelzang. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+import UIKit
+import QuartzCore
+
+class SevenSwitch: UIControl {
+    
+    var on: Bool
+    var activeColor: UIColor
+    var inactiveColor: UIColor
+    var onTintColor: UIColor
+    var offTintColor: UIColor
+    var borderColor: UIColor
+    var thumbTintColor: UIColor
+    var onThumbTintColor: UIColor
+    var shadowColor: UIColor
+    var isRounded: Bool
+    var thumbImage: UIImage
+    var onImage: UIImage
+    var offImage: UIImage
+    var onLabel: UILabel
+    var offLabel: UILabel
+    
+    func setOn(on: Bool, animated: Bool) {
+        
+    }
+    
+    func isOn() -> Bool {
+        
+    }
+}

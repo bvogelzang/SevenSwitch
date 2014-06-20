a4118b1633a927a2f53f1957f34c1e1ce8aa8280
bvogelzang <bvogelzang@breuer.com>
2014-06-20 18:43:44 -0500

    convert methods

--- SevenSwitchExample/SevenSwitchExample/SevenSwitch.swift
+++ SevenSwitchExample/SevenSwitchExample/SevenSwitch.swift
@@ -56,6 +56,9 @@ class SevenSwitch: UIControl {
     var isAnimating: Bool
     var userDidSpecifyOnThumbTintColor: Bool
     
+    /*
+    *   Initialization
+    */
     init() {
         super.initWithFrame(CGRectMake(0, 0, 50, 30))
         setup()
@@ -72,6 +75,10 @@ class SevenSwitch: UIControl {
         setup()
     }
     
+    
+    /*
+    *   Setup the individual elements of the switch and set default values
+    */
     func setup() {
         
         self.on = false
@@ -131,31 +138,246 @@ class SevenSwitch: UIControl {
         thumb.layer.shadowRadius = 2.0
         thumb.layer.shadowOpacity = 0.5
         thumb.layer.shadowOffset = CGSizeMake(0, 3)
-        thumb.layer.shadowPath = UIBezierPath(roundedRect: knob.bounds, cornerRadius: knob.layer.cornerRadius).CGPath;
+        thumb.layer.shadowPath = UIBezierPath(roundedRect: thumb.bounds, cornerRadius: thumb.layer.cornerRadius).CGPath;
         thumb.layer.masksToBounds = false
         thumb.userInteractionEnabled = false
         self.addSubview(thumb)
         
         // thumb image
-        self.thumbImageView = UIImageView(frame: CGRectMake(0, 0, knob.frame.size.width, knob.frame.size.height))
+        self.thumbImageView = UIImageView(frame: CGRectMake(0, 0, thumb.frame.size.width, thumb.frame.size.height))
         thumbImageView.contentMode = UIViewContentMode.Center
         thumbImageView.autoresizingMask = UIViewAutoresizing.FlexibleWidth
         thumb.addSubview(thumbImageView)
     }
     
-    func showOn(animated: Bool) {
+    func beginTrackingWithTouch(touch: UITouch!, withEvent event: UIEvent!) -> Bool {
+        super.beginTrackingWithTouch(touch, withEvent: event)
         
+        startTrackingValue = self.on
+        didChangeWhileTracking = false
+        
+        let activeKnobWidth = self.bounds.size.height - 2 + 5
+        isAnimating = true
+        
+        UIView.animateWithDuration(0.3, delay: 0.0, options: UIViewAnimationOptions.CurveEaseOut | UIViewAnimationOptions.BeginFromCurrentState, animations: {
+                if self.on {
+                    thumb.frame = CGRectMake(self.bounds.size.width - (activeKnobWidth + 1), thumb.frame.origin.y, activeKnobWidth, thumb.frame.size.height)
+                    background.backgroundColor = self.onTintColor
+                    thumb.backgroundColor = self.onThumbTintColor
+                }
+                else {
+                    thumb.frame = CGRectMake(thumb.frame.origin.x, thumb.frame.origin.y, activeKnobWidth, thumb.frame.size.height)
+                    background.backgroundColor = self.activeColor
+                    thumb.backgroundColor = self.thumbTintColor
+                }
+            }, completion: {
+                isAnimating = false
+        })
+        
+        return true
     }
     
-    func showOff(animated: Bool) {
+    func continueTrackingWithTouch(touch: UITouch!, withEvent event: UIEvent!) -> Bool {
+        super.continueTrackingWithTouch(touch, withEvent: event)
         
+        // Get touch location
+        let lastPoint = touch.locationInView(self)
+        
+        // update the switch to the correct visuals depending on if
+        // they moved their touch to the right or left side of the switch
+        if lastPoint.x > self.bounds.size.width * 0.5 {
+            self.showOn(true)
+            if !startTrackingValue {
+                didChangeWhileTracking = true
+            }
+        }
+        else {
+            self.showOff(true)
+            if startTrackingValue {
+                didChangeWhileTracking = true
+            }
+        }
+
+        return true
     }
     
+    func endTrackingWithTouch(touch: UITouch!, withEvent event: UIEvent!) {
+        super.endTrackingWithTouch(touch, withEvent: event)
+        
+        previousValue = self.on
+        
+        if didChangeWhileTracking {
+            self.setOn(currentVisualValue, animated: true)
+        }
+        else {
+            self.setOn(!self.on, animated: true)
+        }
+        
+        if previousValue != self.on {
+            self.sendActionsForControlEvents(UIControlEvent.ValueChanged)
+        }
+    }
+    
+    func cancelTrackingWithEvent(event: UIEvent) {
+        super.cancelTrackingWithEvent(event)
+        
+        // just animate back to the original value
+        if self.on {
+            self.showOn(true)
+        }
+        else {
+            self.showOff(true)
+        }
+    }
+    
+    func layoutSubviews() {
+        super.layoutSubviews()
+        
+        if !isAnimating {
+            let frame = self.frame
+            
+            // background
+            background.frame = CGRectMake(0, 0, frame.size.width, frame.size.height)
+            background.layer.cornerRadius = self.isRounded ? frame.size.height * 0.5 : 2
+            
+            // images
+            onImageView.frame = CGRectMake(0, 0, frame.size.width - frame.size.height, frame.size.height)
+            offImageView.frame = CGRectMake(frame.size.height, 0, frame.size.width - frame.size.height, frame.size.height)
+            self.onLabel.frame = CGRectMake(0, 0, frame.size.width - frame.size.height, frame.size.height)
+            self.offLabel.frame = CGRectMake(frame.size.height, 0, frame.size.width - frame.size.height, frame.size.height)
+            
+            // thumb
+            let normalKnobWidth = frame.size.height - 2
+            if self.on {
+                thumb.frame = CGRectMake(frame.size.width - (normalKnobWidth + 1), 1, frame.size.height - 2, normalKnobWidth)
+            }
+            else {
+                thumb.frame = CGRectMake(1, 1, normalKnobWidth, normalKnobWidth)
+            }
+            
+            thumb.layer.cornerRadius = self.isRounded ? (frame.size.height * 0.5) - 1 : 2
+        }
+    }
+    
+    /*
+    * Set the state of the switch to on or off, optionally animating the transition.
+    */
     func setOn(on: Bool, animated: Bool) {
-        
+        if on {
+            self.showOn(animated)
+        }
+        else {
+            self.showOff(animated)
+        }
     }
     
+    /*
+    *   Detects whether the switch is on or off
+    *
+    *	@return	BOOL YES if switch is on. NO if switch is off
+    */
     func isOn() -> Bool {
-        
+        return self.on
     }
+    
+    /*
+    *   update the looks of the switch to be in the on position
+    *   optionally make it animated
+    */
+    func showOn(animated: Bool) {
+        let normalKnobWidth = self.bounds.size.height - 2
+        let activeKnobWidth = normalKnobWidth + 5
+        if animated {
+            isAnimating = true
+            UIView.animateWithDuration(0.3, delay: 0.0, options: UIViewAnimationOptions.CurveEaseOut | UIViewAnimationOptions.BeginFromCurrentState, animations: {
+                if self.tracking {
+                    thumb.frame = CGRectMake(self.bounds.size.width - (activeKnobWidth + 1), thumb.frame.origin.y, activeKnobWidth, thumb.frame.size.height)
+                }
+                else {
+                    thumb.frame = CGRectMake(self.bounds.size.width - (normalKnobWidth + 1), thumb.frame.origin.y, normalKnobWidth, thumb.frame.size.height)
+                }
+                
+                background.backgroundColor = self.onTintColor
+                background.layer.borderColor = self.onTintColor.CGColor
+                thumb.backgroundColor = self.onThumbTintColor
+                onImageView.alpha = 1.0
+                offImageView.alpha = 0
+                onLabel.alpha = 1.0
+                offLabel.alpha = 0
+            }, completion: {
+                isAnimating = false
+            })
+        }
+        else {
+            if self.tracking {
+                thumb.frame = CGRectMake(self.bounds.size.width - (activeKnobWidth + 1), thumb.frame.origin.y, activeKnobWidth, thumb.frame.size.height)
+            }
+            else {
+                thumb.frame = CGRectMake(self.bounds.size.width - (normalKnobWidth + 1), thumb.frame.origin.y, normalKnobWidth, thumb.frame.size.height)
+            }
+            
+            background.backgroundColor = self.onTintColor
+            background.layer.borderColor = self.onTintColor.CGColor
+            thumb.backgroundColor = self.onThumbTintColor
+            onImageView.alpha = 1.0
+            offImageView.alpha = 0
+            onLabel.alpha = 1.0
+            offLabel.alpha = 0
+        }
+        
+        currentVisualValue = true
+    }
+    
+    /*
+    * update the looks of the switch to be in the off position
+    * optionally make it animated
+    */
+    func showOff(animated: Bool) {
+        let normalKnobWidth = self.bounds.size.height - 2
+        let activeKnobWidth = normalKnobWidth + 5
+        
+        if animated {
+            isAnimating = true
+            UIView.animateWithDuration(0.3, delay: 0.0, options: UIViewAnimationOptions.CurveEaseOut | UIViewAnimationOptions.BeginFromCurrentState, animations: {
+                if self.tracking {
+                    thumb.frame = CGRectMake(1, thumb.frame.origin.y, activeKnobWidth, thumb.frame.size.height);
+                    background.backgroundColor = self.activeColor
+                }
+                else {
+                    thumb.frame = CGRectMake(1, thumb.frame.origin.y, normalKnobWidth, thumb.frame.size.height);
+                    background.backgroundColor = self.inactiveColor
+                }
+                
+                background.layer.borderColor = self.borderColor.CGColor
+                thumb.backgroundColor = self.thumbTintColor
+                onImageView.alpha = 0
+                offImageView.alpha = 1.0
+                self.onLabel.alpha = 0
+                self.offLabel.alpha = 1.0
+                
+            }, completion: {
+                isAnimating = false
+            })
+        }
+        else {
+            if (self.tracking) {
+                thumb.frame = CGRectMake(1, thumb.frame.origin.y, activeKnobWidth, thumb.frame.size.height)
+                background.backgroundColor = self.activeColor
+            }
+            else {
+                thumb.frame = CGRectMake(1, thumb.frame.origin.y, normalKnobWidth, thumb.frame.size.height)
+                background.backgroundColor = self.inactiveColor
+            }
+            backgroundView.layer.borderColor = self.borderColor.CGColor
+            thumb.backgroundColor = self.thumbTintColor
+            onImageView.alpha = 0
+            offImageView.alpha = 1.0
+            onLabel.alpha = 0
+            offLabel.alpha = 1.0
+        }
+        
+        currentVisualValue = false
+    }
+    
+    
 }

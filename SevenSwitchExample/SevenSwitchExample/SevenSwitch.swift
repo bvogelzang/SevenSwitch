7fd8bb7bdf5ff2c09ddf8efaa512219d53f9ab4e
bvogelzang <bvogelzang@breuer.com>
2014-06-20 15:19:26 -0500

    init methods and setup

--- SevenSwitchExample/SevenSwitchExample/SevenSwitch.swift
+++ SevenSwitchExample/SevenSwitchExample/SevenSwitch.swift
@@ -27,6 +27,7 @@ import QuartzCore
 
 class SevenSwitch: UIControl {
     
+    // public
     var on: Bool
     var activeColor: UIColor
     var inactiveColor: UIColor
@@ -42,6 +43,119 @@ class SevenSwitch: UIControl {
     var offImage: UIImage
     var onLabel: UILabel
     var offLabel: UILabel
+    
+    // private
+    var backgroundView: UIView
+    var thumbView: UIView
+    var onImageView: UIImageView
+    var offImageView: UIImageView
+    var thumbImageView: UIImageView
+    var currentVisualValue: Bool
+    var startTrackingValue: Bool
+    var didChangeWhileTracking: Bool
+    var isAnimating: Bool
+    var userDidSpecifyOnThumbTintColor: Bool
+    
+    init() {
+        super.initWithFrame(CGRectMake(0, 0, 50, 30))
+        setup()
+    }
+    
+    init(coder aDecoder: NSCoder!) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    init(frame: CGRect) {
+        var initialFrame: CGRect
+        if CGRectIsEmpty(frame) {
+            initialFrame = CGRectMake(0, 0, 50, 30)
+        }
+        else {
+            initialFrame = frame
+        }
+        super.initWithFrame(initialFrame)
+        setup()
+    }
+    
+    func setup() {
+        
+        self.on = false
+        self.isRounded = true
+        self.currentVisualValue = false
+        self.userDidSpecifyOnThumbTintColor = false
+        self.isAnimating = false
+        
+        // default colors
+        self.inactiveColor = UIColor.clearColor()
+        self.activeColor = UIColor(0.89, 0.89, 0.89, 1)
+        self.onTintColor = UIColor(0.3, 0.85, 0.39, 1)
+        self.borderColor = UIColor(0.78, 0.78, 0.8, 1)
+        self.thumbTintColor = UIColor.whiteColor()
+        self.onThumbTintColor = UIColor.whiteColor()
+        self.shadowColor = UIColor.grayColor()
+        
+        // background
+        self.backgroundView = UIView(frame: CGRectMake(0, 0, self.frame.size.width, self.frame.size.height))
+        backgroundView.backgroundColor = UIColor.clearColor()
+        backgroundView.layer.cornerRadius = self.frame.size.height * 0.5
+        backgroundView.layer.borderColor = self.borderColor.CGColor
+        backgroundView.layer.borderWidth = 1.0
+        backgroundView.userInteractionEnabled = false
+        backgroundView.clipsToBounds = true
+        self.addSubview(backgroundView)
+        
+        // on/off images
+        self.onImageView = UIImageView(frame: CGRectMake(0, 0, self.frame.size.width - self.frame.size.height, self.frame.size.height))
+        onImageView.alpha = 1.0
+        onImageView.contentMode = UIViewContentMode.Center
+        backgroundView.addSubview(onImageView)
+        
+        self.offImageView = UIImageView(frame: CGRectMake(self.frame.size.height, 0, self.frame.size.width - self.frame.size.height, self.frame.size.height))
+        offImageView.alpha = 1.0
+        offImageView.contentMode = UIViewContentMode.Center
+        backgroundView.addSubview(offImageView)
+        
+        // labels
+        self.onLabel = UILabel(frame: CGRectMake(0, 0, self.frame.size.width - self.frame.size.height, self.frame.size.height))
+        onLabel.textAlignment = NSTextAlignment.Center
+        onLabel.textColor = UIColor.lightGrayColor()
+        onLabel.font = UIFont.systemFontOfSize(12)
+        backgroundView.addSubview(onLabel)
+        
+        self.offLabel = UILabel(frame: CGRectMake(self.frame.size.height, 0, self.frame.size.width - self.frame.size.height, self.frame.size.height))
+        offLabel.textAlignment = NSTextAlignment.Center
+        offLabel.textColor = UIColor.lightGrayColor()
+        offLabel.font = UIFont.systemFontOfSize(12)
+        backgroundView.addSubview(offLabel)
+        
+        // thumb
+        self.thumb = UIView(frame: CGRectMake(1, 1, self.frame.size.height - 2, self.frame.size.height - 2))
+        thumb.backgroundColor = self.thumbTintColor
+        thumb.layer.cornerRadius = (self.frame.size.height * 0.5) - 1
+        thumb.layer.shadowColor = self.shadowColor.CGColor
+        thumb.layer.shadowRadius = 2.0
+        thumb.layer.shadowOpacity = 0.5
+        thumb.layer.shadowOffset = CGSizeMake(0, 3)
+        thumb.layer.shadowPath = UIBezierPath(roundedRect: knob.bounds, cornerRadius: knob.layer.cornerRadius).CGPath;
+        thumb.layer.masksToBounds = false
+        thumb.userInteractionEnabled = false
+        self.addSubview(thumb)
+        
+        // thumb image
+        self.thumbImageView = UIImageView(frame: CGRectMake(0, 0, knob.frame.size.width, knob.frame.size.height))
+        thumbImageView.contentMode = UIViewContentMode.Center
+        thumbImageView.autoresizingMask = UIViewAutoresizing.FlexibleWidth
+        thumb.addSubview(thumbImageView)
+    }
+    
+    func showOn(animated: Bool) {
+        
+    }
+    
+    func showOff(animated: Bool) {
+        
+    }
     
     func setOn(on: Bool, animated: Bool) {
         
